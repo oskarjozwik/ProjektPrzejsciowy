27ce609a2e5ca3ba15a88015982c6fd3a2bbd894
progres37 <wiktorgrosy@op.pl>
2026-05-16 14:32:05 +0200

    Integrated sql querying into the main control algorithm

--- raspberry_pi/conveyor_controller/gate_selection/check_database_for_qr.py
+++ raspberry_pi/conveyor_controller/gate_selection/check_database_for_qr.py
@@ -1,18 +1,58 @@
 from package_detection.check_for_package import BoxDimensions
+import mysql.connector
 
 
 class DatabaseCheckResult:
-	def __init__(self, entry_found: bool = True, max_box_dimensions: BoxDimensions = BoxDimensions(0,0,0), max_mass: float = 0, destination: str = ''):
+	def __init__(
+		self,
+		db_error: bool = False,
+		entry_found: bool = True,
+		max_box_dimensions: BoxDimensions = BoxDimensions(0, 0, 0),
+		max_mass: float = 0,
+		destination: str = ''
+	):
+		self.db_error = db_error
 		self.entry_found = entry_found
 		self.max_box_dimensions = max_box_dimensions
 		self.max_mass = max_mass
 		self.destination = destination
 
 
+config = {
+	'user': 'tapedeck',
+	'password': 'Jof_159',
+	'host': '192.168.0.109',
+	'database': 'Tasmociag'
+}
+
+
 def check_database_for_qr(qr_code: int) -> DatabaseCheckResult:
-	print('Warning: check_database_for_qr() is not implemented and returns a constant value')
+	try:
+		db = mysql.connector.connect(**config)
+		cursor = db.cursor(dictionary = True)
+		
+		query = "SELECT * FROM Paczki WHERE id_paczki = %s"
+		cursor.execute(query, (str(qr_code),))
+		query_result = cursor.fetchone()
+		
+		cursor.close()
+		db.close()
+	except Exception as e:
+		return DatabaseCheckResult(
+			db_error = True
+		)
+	
+	if not query_result:
+		return DatabaseCheckResult(
+			entry_found = False
+		)
+	
 	return DatabaseCheckResult(
-		max_box_dimensions = BoxDimensions(0.1, 0.2, 0.15),
-		max_mass = 0.5,
-		destination = 'Warszawa'
+		max_box_dimensions = BoxDimensions(
+			x = query_result['max_box_dimensions_x'],
+			y = query_result['max_box_dimensions_y'],
+			z = query_result['max_box_dimensions_z'],
+		),
+		max_mass = query_result['max_mass'],
+		destination = query_result['destination']
 	)
--- raspberry_pi/conveyor_controller/main.py
+++ raspberry_pi/conveyor_controller/main.py
@@ -20,13 +20,14 @@ def main():
 			if package_check_result.box_detected and package_check_result.qr_detected:
 				is_conveyor_empty = False
 				
-				requested_gate = select_gate_for_package(
+				gate_selection_result = select_gate_for_package(
 					mass = package_check_result.mass,
 					box_dimensions = package_check_result.box_dimensions,
 					qr_code = package_check_result.qr_code,
 					gate_destinations = gate_destinations,
 				)
-				transport_package_to_gate( requested_gate )
+				print(gate_selection_result.info)
+				transport_package_to_gate( gate_selection_result.gate )
 		else:
 			if has_package_just_arrived():
 				is_conveyor_empty = True
