4f4e6301bb28a7d1838d4f12028010ccd32407de
progres37 <wiktorgrosy@op.pl>
2026-05-18 16:28:00 +0200

    Added code for interacting with google maps (still needs a valid API key)

--- raspberry_pi/conveyor_controller/gate_selection/select_closest_location.py
+++ raspberry_pi/conveyor_controller/gate_selection/select_closest_location.py
@@ -1,6 +1,25 @@
+import googlemaps
+from googlemaps.distance_matrix import distance_matrix
+
+api_key = 'Enter API key here'
+client = googlemaps.Client(key = api_key)
+
 def select_closest_location(
 	target: str,
 	available: list[str]
-):
-	print('Warning: select_closest_location() is not implemented and returns the first value from the list')
-	return available[0]
+) -> str:
+	print('Warning: select_closest_location() needs a Google Maps API key to work properly')
+	
+	matrix = googlemaps.distance_matrix.distance_matrix(
+		client = client,
+		origins = available,
+		destinations = [target],
+		mode = 'driving'
+	)
+	
+	distances = []
+	for row in matrix['rows']:
+		distances.append(row['elements'][0]['duration']['value'])
+	closest_location = available[distances.index(min(distances))]
+	
+	return closest_location
